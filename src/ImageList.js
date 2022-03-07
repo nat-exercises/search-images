8d51b250d466c602bce78e59c2ba29f217d3eec9
natacha <nat_bartu@hotmail.com>
2022-03-07 13:29:44 +1000

    created and initialize app with SearchBar.js and ImageList.js

---
+++ src/ImageList.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class ImageList extends Component {
+  render() {
+    return <div>ImageList</div>;
+  }
+}
+
+export default ImageList;
---
+++ src/SeachBar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class SeachBar extends Component {
+  render() {
+    return <div>SeachBar</div>;
+  }
+}
+
+export default SeachBar;
